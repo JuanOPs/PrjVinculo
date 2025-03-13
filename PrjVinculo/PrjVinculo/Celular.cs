dda7a6df02fcc8a78b5cb9c5344eb4dadf69df1d
aluno <aluno@aluno.com>
2025-03-12 21:48:28 -0300

    Adicionar arquivos de projeto.

---
+++ PrjVinculo/PrjVinculo/Celular.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrjVinculo
+{
+    class Celular
+    {
+        public string marca { get; set; }
+        public string numero { get; set; }
+        public string operadora { get; set; }
+
+    }
+}
---
+++ PrjVinculo/PrjVinculo/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace PrjVinculo
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void btnProduto_Clicked(object sender, EventArgs e)
+        {
+            Produto Produto = new Produto();
+
+            Produto.Descricao = "Garrafa";
+            Produto.Preco = "R$ 50,00";
+            Produto.Categoria = "Garrafas";
+
+            Navigation.PushAsync(new ProdutoPage() { BindingContext = Produto});
+        }
+    }
+
+}
